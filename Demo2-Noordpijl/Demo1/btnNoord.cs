2ce3f10ff33348975f306a6065910b85dfd3302e
Peter Mallo <pmallo@esri.nl>
2016-05-12 09:13:59 +0200

    demo 2 - Noordpijl uitgebreid

---
+++ Demo2-Noordpijl/Demo1/btnNoord.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+
+namespace Demo1
+{
+    internal class btnNoord : Button
+    {
+        protected override void OnClick()
+        {
+            QueuedTask.Run(() =>
+            {
+                //Get the active map view.
+                var mapView = MapView.Active;
+                if (mapView == null)
+                    return false;
+
+                //Get the camera for the view, adjust the heading and zoom to the new camera position.
+                var camera = mapView.Camera;
+                camera.Heading = 0;
+                camera.Pitch = -90;
+                return mapView.ZoomTo(camera, TimeSpan.Zero);
+            });
+        }
+    }
+}
