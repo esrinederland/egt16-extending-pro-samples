3e68e605998e85a2033a6d0a4d7ea1d9e636e00b
Peter Mallo <pmallo@esri.nl>
2016-05-12 08:36:20 +0200

    Demo 2 - loading events

---
+++ Demo2-Loading/Demo2-Loading/Button1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using System.Windows;
+
+namespace Demo2_Loading
+{
+    internal class Button1 : Button
+    {
+        public Button1()
+        {
+            ArcGIS.Desktop.Framework.Dialogs.MessageBox.Show("Button 1 is nu geladen", "Button 1 geladen", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        protected override void OnClick()
+        {
+        }
+    }
+}
